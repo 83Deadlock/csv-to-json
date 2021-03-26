5bfc4a3cc9251ef38a64acc7022051ac7383cbed
José Pedro Ferreira <zepedrocferreira@gmail.com>
2021-03-26 10:49:29 +0000

    defined function resolving function; solved unreadable * char on input; missing output strategy

--- csv2json.py
+++ csv2json.py
@@ -3,15 +3,53 @@ import re
 
 f = open("test.csv","r",encoding="utf-8")
 
-headers = re.split(r'\W+',f.readline())
+headers = re.split(r';|\n',f.readline())
+headers.pop()
+notas = []
+func = ""
+nota = 0
 
-print("[")
+def funcDef(func,notas):
+    n = len(notas)
+    total = 0
+    if func == "sum":
+        for i in range(n):
+            total = total + notas[i]
+    elif func == "avg":
+        total = funcDef("sum",notas)/len(notas)
+    elif func == "max":
+        total = max(notas)
+    elif func == "min":
+        total = min(notas)
+    else:
+        total = -1
+    return total
+
 for line in f:
+    i = 4
     res = re.split(r'\W+',line)
     res.pop()
-    print(res)
 
-headers.pop()
-print(headers)
+    aux = re.split(r'\*|\n',headers[3]) 
+
+    if '*' in headers[3]:
+        aux = re.split(r'\*|\n',headers[3])
+
+        if aux[len(aux)-1] == '':
+            aux.pop()
+            func = aux[0]
+            i = 4
+            while i < len(res):
+                notas.append(int(res[i]))
+                i = i+1
+        else:
+            func = aux[0] + aux[1]
+            i = 4
+            while i < len(res):
+                notas.append(int(res[i]))
+                i = i+1
+            nota = funcDef(aux[1],notas)
+    else:
+        func = headers[3]
 
 print("]")
